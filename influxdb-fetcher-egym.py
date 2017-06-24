e03d90583a89ba57e169c827dc765c361d694a50
bitstacker <bitstacker@never-afk.de>
2017-06-24 22:27:10 +0200

    works now

--- influxdb-fetcher-egym.py
+++ influxdb-fetcher-egym.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python
 
 import yaml
 import egym
-import influxdb
+from influxdb import InfluxDBClient
+from datetime import date
+from datetime import timedelta
 
 class InfluxDbFetcherEgym(object):
     def __init__(self):
         self.config = self.loadConfig()
+        self.fetchEgymData()
 
     def loadConfig(self):
         with open("config.yml", 'r') as stream:
@@ -16,7 +20,30 @@ class InfluxDbFetcherEgym(object):
         return y
 
     def fetchEgymData(self):
-        pass
+        api = egym.Api(email=self.config['egym_email'],
+                      password=self.config['egym_password'])
+        sessions = api.GetUserSessions(date.today() - timedelta(days=7), date.today())
+        for session in sessions:
+            data = api.GetSessionData(session)
+            points = data.getPoints()
+            isodate = data.getIsoDate()
+            self.addToInfluxDb(isodate, points)
+
+    def addToInfluxDb(self, isodate, points):
+        json_body = [{
+                        "measurement": "egym",
+                        "time": isodate,
+                        "fields": {
+                            "value": points
+                            }
+                    }]
+        client = InfluxDBClient(self.config['influx_host'], 
+                                self.config['influx_port'], 
+                                self.config['influx_user'],
+                                self.config['influx_password'],
+                                self.config['influx_db'],
+                                )
+        client.write_points(json_body)
 
 if __name__ == "__main__":
     f = InfluxDbFetcherEgym()
